b785462fbb75a0087650edc5eaea41ceec1abf2f
HaoChen Wang <x4z9a@bowen.ugrad.cs.ubc.ca>
2016-09-12 15:44:09 -0700

    first commit

---
+++ q1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+float circle_area(float radius);  // declare function before main()
+
+int main(void) {
+  float circle_radius;
+  std::cout << "Enter radius:" << std::endl;
+  std::cin >> circle_radius;
+  std::cout << "Area is: " << circle_area(circle_radius) << std::endl;
+  return 0;
+}
+
+float circle_area(float radius) {
+  return 3.14159 * radius * radius;  // = pi * r^2
+}
